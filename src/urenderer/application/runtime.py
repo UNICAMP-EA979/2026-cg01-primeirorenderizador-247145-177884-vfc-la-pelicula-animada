0402ccb1d9a1c9caa07a05cfd6fbcf710de88136
henrique <hemofilho85@gmail.com>
2026-05-13 01:04:02 -0300

    Iniciando grandchild.py

--- src/urenderer/application/runtime.py
+++ src/urenderer/application/runtime.py
@@ -74,13 +74,10 @@ class Runtime:
 
             # Traverse the node children
             for child in node.children:
-                ## SEU CÓDIGO AQUI #####################################################
                 # Crie a transformação do nó filho, concatenando com as transformações anteriores
 
                 # Create child transformation
                 child_transformation = np.matmul(transformation, child.model_transform)
-
-                #########################################################################
 
                 # Add child to the processing queue
                 nodes.append((child, child_transformation))
